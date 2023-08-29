ef253510fa11503def33f733e96614c7b660ce2f
Aleksandra <abaibik@gmail.com>
2023-08-29 15:49:01 +0200

    data to console

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.formEntries(formData);
+
+  console.log(data);
+
+  event.target.reset();
+
+  event.target.elements.firstName.focus();
+});
